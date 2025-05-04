cc906fd9707bc253e2f58b10afe6add11a395463
mowebiaz <mowebiaz@gmail.com>
2025-05-04 11:43:47 +0200

    Add animations to linksContainer

--- app/liens/LinksContainer/LinksContainer.jsx
+++ app/liens/LinksContainer/LinksContainer.jsx
@@ -1,24 +1,41 @@
+'use client'
+
+import { AnimatePresence, motion } from 'motion/react'
 import { LinkAria } from '@/src/components/LinkAria/LinkAria'
 import './LinksContainer.scss'
 
 export function LinksContainer({ links, selectedCategories }) {
   return (
-    <div className="cards">
-      {links
-        .filter((link) => selectedCategories.includes(link.category))
-        .map((link) => (
-          //<div key={link.name} data-linkcategory={link.category}>
-            <LinkAria
-            key={link.name} data-linkcategory={link.category}
-              href={link.link}
-              className="card"
-              target="_blank"
-              referrerPolicy='no-opener no-referrer'
+    <motion.div
+      className="cards"
+      layout
+      style={{ position: 'relative' }}
+    >
+      <AnimatePresence mode="popLayout">
+        {links
+          .filter((link) => selectedCategories.includes(link.category))
+          .map((link) => (
+            <motion.div
+              key={link.name}
+              layout
+              initial={{ opacity: 0, y: -10 }}
+              animate={{ opacity: 1, y: 0 }}
+              exit={{ opacity: 0, y: 10 }}
+              transition={{ duration: 0.2 }}
             >
-              {link.name}
-            </LinkAria>
-          //</div>
-        ))}
-    </div>
+              <LinkAria
+                key={link.name}
+                data-linkcategory={link.category}
+                href={link.link}
+                className="card"
+                target="_blank"
+                referrerPolicy="no-opener no-referrer"
+              >
+                {link.name}
+              </LinkAria>
+            </motion.div>
+          ))}
+      </AnimatePresence>
+    </motion.div>
   )
 }
